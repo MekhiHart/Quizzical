6405bbeecbd07435124b42a0dd9d635598952975
MekhiHart <mekhihartdelacruz@gmail.com>
2023-01-04 15:57:07 -0800

    Done starting screen

---
+++ src/Components/StartingScreen.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default function StartingScreen(props){
+
+    return(
+        
+        <div className="starting-div">
+            <img className="start-logo" src={props.logo}/>
+            <h1>Quizzical</h1>
+            <p>Anime quizes!</p>
+            <button className="createQuiz-btn"> Start Quiz </button>
+        </div>
+    )
+}
