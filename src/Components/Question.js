8745e3facb5f07a36899aee785be36092fafa01b
MekhiHart <mekhihartdelacruz@gmail.com>
2023-01-05 15:46:12 -0800

    Points system, and selecting buttons implemented

--- src/Components/Question.js
+++ src/Components/Question.js
@@ -1,14 +1,49 @@
 import {nanoid} from "nanoid"
 export default function Question(props){
 
+    const question = props.data.question
+    const questionKey = props.data.id
+
+    // * Render Button
+
+    const buttons = props.data.buttons.map(button => {
+    
+        const buttonStyle={
+            background: button.isSelected ? "#D6DBF5" : "none",
+            borderRadius: "7.94239px",
+            border:button.isSelected ? "none" : "0.794239px solid #4D5B9E",
+
+            display: "flex",
+            padding: "10px 20px",
+            marginRight: "20px",
+            marginBottom: "10px",
+            outline: "none",
+          
+            fontFamily: 'Inter',
+            fontStyle: "normal",
+            fontWeight:"500",
+            fontSize: "10.24px",
+            lineHeight: "12px",
+            textAlign: "center",
+        }
+    
+        return (
+        <button 
+        key={button.id}
+        onClick={() => props.selectButton(questionKey,button.id)}
+        style={buttonStyle}
+        >{button.value}</button> )})
+
 
 
     return(
         <div className="question">
-            <h1>Hello</h1>
+            <h1>{question}</h1>
             <div className="answers">
-                <button> Yay </button>
+                {buttons}
             </div>
+            <div className="break-line"/>
         </div>
+        
     )
 }
--- src/Components/QuizScreen.js
+++ src/Components/QuizScreen.js
@@ -2,20 +2,66 @@ import React, {useState,useEffect} from "react";
 import Question from "./Question"
 import {nanoid} from "nanoid"
 
-export default function Quiz(props){
+export default function QuizScreen(props){
 
     const [questionsObjArray, setQuestionsObjArray] = useState([])
+    const [quizPoints, setQuizPoints] = useState(0)
 
     useEffect(()=>{
         fetch("https://opentdb.com/api.php?amount=5&category=31&encode=base64")
         .then(res => res.json())
         .then(data => setQuestionsObjArray(transformData(data))) // ! Use atob() to decode data
-
     },[]) // * useEffect
 
-    console.log(questionsObjArray)
+    const questionComponents = questionsObjArray.map(questionObj => (
+        <Question 
+        id={questionObj.id}
+        key={questionObj.id}
+        data={questionObj}
+        selectButton={selectButton} // * Function
+        />))
+
+    // * Interactive functions
+
+    function selectButton(questionId, buttonId){ // ! Fuct this func lmao
+
+        setQuestionsObjArray(prevState=>{
+            let returnArray // ! Contains array of objects
+            returnArray = prevState.map(questionObj =>{
+                if(questionObj.id === questionId ){ // * If the question component is what the user clicked
+                    return{
+                        ...questionObj,
+                        buttons: questionObj.buttons.map(button =>({
+                            ...button,
+                            isSelected: buttonId === button.id
+                        }))
+                    }
+                }
+
+                else return questionObj // * If the question component is not the user clicked
+                 // *temp
+            })
+
+           setQuizPoints( HandleQuizPoints(returnArray)) // ! Sets points
+            // returnButtonProps(questionId,buttonId,returnArray)
+            return returnArray // * New Array with button isSelected
+        })
+    }// *selectButton()
+    
+    // * Interactive functions
+
+    function HandleQuizPoints(arr){ // ! Fuct this func lmao
+        let local_quizPoints = 0
+        arr.forEach(questionObj =>{
+            questionObj.buttons.forEach(button => {
+                if(button.isSelected && button.isCorrect) local_quizPoints++
+            })
+        })
+        return local_quizPoints
+    }
 
     
+
     function transformData(data){ // ! For data.result
         const results = data.results
 
@@ -32,8 +78,9 @@ export default function Quiz(props){
             }))
 
             questionObj = {
-                id: nanoid(),
-                button: buttonsObjArray
+                question: atob(questionObj.question),
+                buttons: buttonsObjArray,
+                id:nanoid()
             }
             return questionObj
         })
@@ -65,6 +112,8 @@ export default function Quiz(props){
     } //* shuffleArray
 
     return(
-        <Question/>
+        <div>
+            {questionComponents}
+        </div>
     )
 }
